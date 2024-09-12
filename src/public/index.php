fc2771f688e325a2ac308e0996c17f4c1c8f8f08
Rajesh Budhathoki <rajoyish@gmail.com>
2024-09-12 17:48:35 +0545

    Coding Standards, Autoloading (PSR-4) & Composer

--- src/public/index.php
+++ src/public/index.php
@@ -1,15 +1,16 @@
 <?php
-require_once '../PaymentGateway/Stripe/Transaction.php';
-require_once '../PaymentGateway/Paddle/Transaction.php';
-require_once '../PaymentGateway/Paddle/CustomerProfile.php';
-require_once '../Notification/Email.php';
 
-// use PaymentGateway\Paddle\{Transaction, CustomerProfile};
-use PaymentGateway\Paddle;
-use PaymentGateway\Stripe\Transaction as StripeTransaction;
+use Ramsey\Uuid\UuidFactory;
 
-$paddleTransaction = new Paddle\Transaction();
-$stripTransaction = new StripeTransaction();
-$paddleCustomerProfile = new Paddle\CustomerProfile();
+require __DIR__ . '/../vendor/autoload.php';
 
-var_dump($paddleTransaction, $stripTransaction, $paddleCustomerProfile);
+use App\PaymentGateway\Paddle\Transaction;
+
+$paddleTransaction = new Transaction();
+
+var_dump($paddleTransaction);
+
+echo "<hr>";
+
+$id = new UuidFactory();
+echo $id->uuid4();
